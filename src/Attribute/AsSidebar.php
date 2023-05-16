eecae9f147b179c09be1d619d2771d042e4e27ed
Maxim Smakouz <m.smakouz@gmail.com>
2023-05-17 00:11:29 +0300

    Add Sidebar listener

---
+++ src/Attribute/AsSidebar.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\AdminPanel\Attribute;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+final class AsSidebar
+{
+}
---
+++ src/Menu/SidebarListener.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\AdminPanel\Menu;
+
+use Psr\Container\ContainerInterface;
+use Spiral\AdminPanel\Attribute\AsSidebar;
+use Spiral\Attributes\ReaderInterface;
+use Spiral\KnpMenu\MenuInterface;
+use Spiral\KnpMenu\MenuRegistry;
+use Spiral\Tokenizer\TokenizationListenerInterface;
+
+final class SidebarListener implements TokenizationListenerInterface
+{
+    public function __construct(
+        private readonly ReaderInterface $reader,
+        private readonly MenuRegistry $registry,
+        private readonly ContainerInterface $container
+    ) {
+    }
+
+    public function listen(\ReflectionClass $class): void
+    {
+        $attr = $this->reader->firstClassMetadata($class, AsSidebar::class);
+
+        if ($attr !== null && !$this->registry->has('zentlix.admin.sidebar')) {
+            $menu = $this->container->get($class->getName());
+
+            \assert($menu instanceof MenuInterface);
+
+            $this->registry->add('zentlix.admin.sidebar', $menu);
+        }
+    }
+
+    public function finalize(): void
+    {
+    }
+}
