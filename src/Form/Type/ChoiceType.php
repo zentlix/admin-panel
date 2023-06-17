61e9d266608c6c2eb2655a23e0cd0346701a58dd
Maxim Smakouz <m.smakouz@gmail.com>
2023-06-17 14:27:54 +0300

    Add form types

--- src/Form/Type/ChoiceType.php
+++ src/Form/Type/ChoiceType.php
@@ -4,22 +4,18 @@ declare(strict_types=1);
 
 namespace Spiral\AdminPanel\Form\Type;
 
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType as BaseCheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType as BaseChoiceType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 
-class CheckboxType extends BaseCheckboxType
+class ChoiceType extends BaseChoiceType
 {
     public function buildView(FormView $view, FormInterface $form, array $options): void
     {
         parent::buildView($view, $form, $options);
 
-        if (empty($view->vars['label_attr']['class'])) {
-            $view->vars['label_attr']['class'] = 'custom-control-label';
-        }
-
         if (empty($view->vars['attr']['class'])) {
-            $view->vars['attr']['class'] = 'custom-control-input';
+            $view->vars['attr']['class'] = 'form-control';
         }
     }
 }
---
+++ src/Form/Type/CollectionType.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\AdminPanel\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\CollectionType as BaseCollectionType;
+
+class CollectionType extends BaseCollectionType
+{
+}
