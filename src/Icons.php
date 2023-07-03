3cf8dbab1a4e70cde3f2d326a5f2304bbf15b430
Maxim Smakouz <m.smakouz@gmail.com>
2023-07-04 01:11:01 +0300

    Add icons

--- src/Icons.php
+++ src/Icons.php
@@ -7,9 +7,12 @@ namespace Spiral\AdminPanel;
 enum Icons: string
 {
     case m_cog_8_tooth = 'm-cog-8-tooth';
+    case m_globe_alt = 'm-globe-alt';
     case m_home = 'm-home';
     case m_user_group = 'm-user-group';
     case o_cog_8_tooth = 'o-cog-8-tooth';
+    case o_globe_alt = 'o-globe-alt';
     case o_home = 'o-home';
     case o_user_group = 'o-user-group';
+    case s_globe_alt = 's-globe-alt';
 }
